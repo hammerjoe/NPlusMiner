6e56cc65d9b9e317f155c59e8d1b8265505898f2
hammerjoe <hammerjoe@gmail.com>
2021-12-12 01:42:03 -0400

    Update lolminer.ps1
    added fees and algos

--- Miners/lolminer.ps1
+++ Miners/lolminer.ps1
@@ -6,15 +6,35 @@ $Uri = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.17/l
 
 $Commands = [PSCustomObject]@{
     # "equihash96" = " --coin MNX" #Equihash 96,5
-    #"Equihash21x9" = "--coin AION" #Equihash 210,9
-    # "equihash144" = " --coin AUTO144_5" #Equihash 144,5
-    # "GrinCuckatoo32" = " --coin GRIN-C32" #Equihash 144,5
-    # "beam" = " --coin BEAM" #Equihash 150,5
+    "Equihash21x9" = "--coin AION" #Equihash 210,9
+    "equihash144" = " --coin AUTO144_5" #Equihash 144,5
+    "equihash192x7" = " --coin AUTO192_7"
+    "equihash144x5" = " --coin BTCZ"
+    "equihash144x5I = " --coin BTG"
+    "GrinCuckatoo32" = " --coin GRIN-C32" #Equihash 144,5
+    "beam" = " --coin BEAM" #Equihash 150,5
+    "cortex" = " --coin CTXC"
+    "ethash" = " --coin ETH"
+    "etchash" = " --coin ETC"
+    "grinc29" = " --coin GRIN-C29M"
+    "grinc32" = " --coin GRIN-C32"
+    "mwcc29d" =" --coin MWC-C29D"
+    "mwcc31" =" --coin MWC-C31"
+    "equihash144x5ii" =" --coin XSG"
+    "zelhash" =" --coin ZEL
     }
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 $WinningCustomConfig = [PSCustomObject]@{}
 
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+    $fee = switch ($_){
+        "octopus"   {0.02}
+        "etchash"   {0.007}  
+        "ethash"    {0.007}
+        "cortex"    {0.025}
+        default     {0.01}
+    }
+    
     $Algo =$_
     $AlgoNorm = Get-Algorithm($_)
 
@@ -28,7 +48,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
             Type      = "NVIDIA"
             Path      = $Path
             Arguments =  Merge-Command -Slave $Arguments -Master $CustomCmdAdds -Type "Command"
-            HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Week * 0.99} # 1% dev fee
+            HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Week * (1-$fee) # 1% dev fee
             API       = "LOL"
             Port      = $Variables.NVIDIAMinerAPITCPPort
             Wrap      = $false
@@ -43,7 +63,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
             Type      = "AMD"
             Path      = $Path
             Arguments =  Merge-Command -Slave $Arguments -Master $CustomCmdAdds -Type "Command"
-            HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Week * 0.99} # 1% dev fee
+            HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Week *(1-$fee)} # dev fee
             API       = "LOL"
             Port      = $Variables.AMDMinerAPITCPPort
             Wrap      = $false
